5fcc330b204d4baabd3286e6d4a70447ac8b024d
Thành Thái <79505243+thaivo02@users.noreply.github.com>
2021-10-30 22:56:04 +0700

    Motor Control
    since 2016

---
+++ dieu_khien_dong_co_360.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+#include <thButton.h>
+
+Servo myservo;
+Servo gservo;
+
+thButton Button (2);
+
+int pos = 0;
+
+void setup ()
+{
+ Serial.begin(9600);
+ Serial.println();
+ 
+ myservo.attach(8);
+ gservo.attach (9);
+}
+
+void loop ()
+{
+ if (Button.isPress())
+  { Serial.println (pos);
+   Serial.print("tang len 30 do");
+    pos += 30;
+  myservo.write(pos);
+  gservo.write(pos);
+  }
+}
